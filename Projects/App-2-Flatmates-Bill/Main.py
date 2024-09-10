bc9f82325bf340225ee4eb614c1a6cd00325e952
RAHUL  KASHYAP <162086635+RahulKashyap1996@users.noreply.github.com>
2024-09-10 20:59:35 +0530

    Fixed the class issue for the input files

--- Projects/App-2-Flatmates-Bill/Main.py
+++ Projects/App-2-Flatmates-Bill/Main.py
@@ -25,7 +25,11 @@
 
 
 
-class Bill: #Class bill is used for initializing the objects amount and period
+class Bill:
+    """
+    Bill amount is an object that has two parameters Bill Amount and Bill Period and
+    a method payment that calculates the amount that everyone has to pay
+    """
     def __init__(self,amount,period):   #initialization method with two parameters amount and period
         self.amount=amount
         self.period=period
@@ -33,56 +37,60 @@ class Bill: #Class bill is used for initializing the objects amount and period
     def payment(self,flatmate): #Method Payment it is used to calculate the payment per flatmate according to their days of stay
         return flatmate.days_stayed*(self.amount/self.period)
 
-class Flatmate: #the flatmate class has parameter name and days stayed
-    def __init__(self,name,days_stayed):    #initialization of parameters name and day stayed
-        try:
-            self.name = input("Enter the name of the first flatmate:\n")
-        except  ValueError as e_msg:
-            print(e_msg)
+class Flatmate:
+    """
+    The object flatmate is has two parameters name and days stayed which
+    """
+    def __init__(self,name,days_stayed):
+        self.name=name       #initialization of parameters name and day stayed
+        self.days_stayed=days_stayed
 
-        try:
-            self.days_stayed = int(input(f"Enter the days stayed of the {self.name}:\n"))
-            if self.days_stayed < 0:
-                raise ValueError("Please Enter the days correctly since it's value should be positive.")
-        except ValueError as e_msg:
-            print(e_msg)
 
     def print_amount_paid(self,bill1):   # this method gives the total amount paid for each flatmate separately
         print(f"Flat mate {self.name}  paid {bill1.payment(self)}") # for this you can call self directly for the class
 
-    def input_method_name(self):
+def get_valid_flatmate_input():
+    while True:
         try:
-            self.name=input("Enter the name of the first flatmate:\n")
-        except  ValueError as e_msg:    #ok
-            print(e_msg)
+            name=input("Enter the name of the  flatmate:\n")
+            days_stayed=int(input(f"Enter the days stayed for the {name}:\n"))
 
-    def input_method_days_stayed(self):
+            if days_stayed < 0:
+                raise ValueError(f"Please Enter Positive Value for Days Stayed for {name}")
+            return Flatmate(name, days_stayed)
+
+        except ValueError as er:
+            print (f"THe error is {er}")
+
+
+def get_valid_bill_input():
+    while True:
         try:
-            self.days_stayed=int(input(f"Enter the days stayed of the {self.name}:\n"))
-            if self.days_stayed<0:
-                raise ValueError("Please Enter the days correctly since it's value should be positive.")
-        except ValueError as e_msg:
-                print(e_msg)
+            bill_amount = float(input("Enter the Bill Amount:\n"))
+            bill_period = int(input("Enter the billing period (e.g., December 2020):\n"))
+
+            if bill_amount < 0:
+                raise ValueError(f"Please Enter Positive Value for the Bill Amount")
+            if bill_period < 0:
+                raise ValueError(f"Please Enter Positive Value for the Bill Period")
+
+            return Bill(bill_amount, bill_period)
+
+        except ValueError as er:
+            print(f"The error message is {er}")
 
 try:
-    flatmate1=Flatmate(Flatmate.input_method_name(),Flatmate.input_method_days_stayed())
-    flatmate2=Flatmate(input("Enter the name of the Second flatmate:\n"),int(input("Enter the days stayed of the Second flatmate:\n")))
-
-    if flatmate2.days_stayed<0:
-        raise ValueError(f"Please Enter Positive Value for Days Stayed for {flatmate2.name}")
-    elif flatmate1.days_stayed<0:
-        raise ValueError(f"Please Enter Positive Value for Days Stayed for {flatmate1.name}")
-
-    bill=Bill(float(input("Enter the total bill amount:\n")),int(input("Enter the billing period (e.g., December 2020):\n")))
-
-    if bill.amount<0:
-        raise ValueError("Please Enter Positive Values for Bill Amount")
-    elif bill.period<0:
-        raise ValueError("Please Enter Positive Values for Bill Period")
-
+    flatmate1=get_valid_flatmate_input()
+    flatmate2=get_valid_flatmate_input()
+    bill=get_valid_bill_input()
     flatmate1.print_amount_paid(bill)
     flatmate2.print_amount_paid(bill)
 
-except ValueError as e:
-    print(f"The error is {e}")
+except ValueError as err_msg:
+    print(f"The error message is {err_msg}")
 
+try:
+    fpdf=FPDF()
+    fpdf.
+except ValueError as er:
+    print(f"The error message is {er}")
