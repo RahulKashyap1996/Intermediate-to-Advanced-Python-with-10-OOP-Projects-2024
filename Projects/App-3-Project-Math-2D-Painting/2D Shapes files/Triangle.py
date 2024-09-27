82b9c62c59dad3e48d8bd3cba58614a68088cb5e
RAHUL  KASHYAP <162086635+RahulKashyap1996@users.noreply.github.com>
2024-09-27 19:48:39 +0530

    27th Sep

--- Projects/App-3-Project-Math-2D-Painting/2D Shapes files/Triangle.py
+++ Projects/App-3-Project-Math-2D-Painting/2D Shapes files/Triangle.py
@@ -1,3 +1,8 @@
+from math import sin,cos,acos,asin,atan
+
+
+
+
 
 class Triangle:
     def __init__(self,a=0,b=0,c=0,angA=0,angB=0,angC=0,type=""):
@@ -10,37 +15,86 @@ class Triangle:
         self.angC=angC
         self.type=type
 
-    def print_func_one(self):
-        print("Please Go through the instructions given and enter the correct values:")
+    def cosine_rule_find_angA(self):
+        self.angA=acos((self.b**2+self.c**2-self.a**2)/2*self.b*self.c)
+        return self.angA
+
+    def cosine_rule_find_angB(self):
+        self.angB=acos((self.a**2+self.c**2-self.b**2)/2*self.a*self.c)
+        return self.angB
+
+    def cosine_rule_find_angC(self):
+        self.angC=acos((self.a**2+self.b**2-self.a**2)/2*self.b*self.c)
+        return self.angC
+
+    def print_all_sides_and_angles(self):
+        print(f"""
+                The side a of the triangle is {self.a}
+                The side b of the triangle is {self.b}  
+                The side c of the triangle is {self.c}
+                The Angle A of the triangle is {self.angA}
+                The Angle B of the triangle is {self.angB}
+                The Angle C of the triangle is {self.angC}
+        """)
+
+    def find_type_of_triangle_on_sides(self):
+        self.find_all_sides_and_angles()
+        if self.a==self.b==self.c or self.angA==self.angB==self.angC==60:
+            return "Equilateral Triangle"
+
+        if self.a==self.b and self.angA==self.angB or\
+           self.b==self.c and self.angB==self.angC or\
+           self.c==self.a and self.angA==self.angC:
+            return "Isosceles Triangle"
+
+
+
+        if self.a!=self.b and self.b!=self.c:
+            return "Isosceles Triangle"
+
+    def find_type_of_triangle_on_angle(self):
+        self.find_all_sides_and_angles()
+        if self.angA == 90 or self.angB == 90 or self.angC == 90:
+            return "Right Angled Triangle"
+
+        if self.angA <= 90 and self.angB <= 90 and self.angC <= 90:
+            return  "Acute Angled Triangle"
+
+        if self.angA >= 90 or self.angB >= 90 and self.angC >= 90:
+            return  "Obtuse Angled Triangle"
+
+        if self.angA == 60 and self.angB == 60 and self.angC == 60:
+            return  "Equilateral Triangle"
+
 
     def check_the_input(self):
-        if(self.type=="e".lower() or self.type=="e".upper()):
+        if self.type== "e".lower() or self.type== "e".upper():
             if ((self.a!=0 and self.b==0 and self.c==0) or
                 (self.a==0 and self.b!=0 and self.c==0) or
                 (self.a==0 and self.b==0 and self.c!=0)):
                 return "You have entered the values for an equilateral Triangle"
             else:
-                self.print_func_one()
+                print_func_one()
                 input_requirements_to_draw_triangle()
 
-        if (self.type == "i".lower() or self.type == "i".upper()):
+        if self.type == "i".lower() or self.type == "i".upper():
             if((self.a!=0 and self.b!=0 and self.c==0 and self.angA==0 and self.angB==0 and self.angC==0) or
                 (self.a==0 and self.b!=0 and self.c==0 and self.angA!=0 and self.angB==0 and self.angC==0)):
                 return "You have entered the Correct values for an Isosceles Triangle"
             else:
-                self.print_func_one()
+                print_func_one()
                 input_requirements_to_draw_triangle()
 
-        if (self.type == "r".lower() or self.type == "r".upper()):
+        if self.type == "r".lower() or self.type == "r".upper():
             if ((self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
                 (self.a == 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
                 (self.a == 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0)):
                 return "You have entered the Correct values for an Right Angle Triangle"
             else:
-                self.print_func_one()
+                print_func_one()
                 input_requirements_to_draw_triangle()
 
-        if(self.type==""):
+        if self.type== "":
                 #for SSS
             if ((self.a != 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
                 #for SAS
@@ -60,62 +114,141 @@ class Triangle:
                 (self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC != 0)):
                 return "You have entered the Correct values for the Triangle"
             else:
-                self.print_func_one()
+                print_func_one()
                 input_requirements_to_draw_triangle()
 
     def find_all_sides_and_angles(self):
-        if (self.type == "e".lower() or self.type == "e".upper()):
+        if self.type == "e".lower() or self.type == "e".upper():
             self.angA=self.angB=self.angC=60
-            if (self.a != 0 and self.b == 0 and self.c == 0):
+            if self.a != 0 and self.b == 0 and self.c == 0:
                 self.b=self.c=self.a
 
-            if (self.a == 0 and self.b != 0 and self.c == 0):
-                self.a=self.c=self.self.b
-                (self.a == 0 and self.b == 0 and self.c != 0)):
-                return "You have entered the values for an equilateral Triangle"
-            else:
-                self.print_func_one()
-                input_requirements_to_draw_triangle()
+            if self.a == 0 and self.b != 0 and self.c == 0:
+                self.a=self.c=self.b
 
-        if (self.type == "i".lower() or self.type == "i".upper()):
-            if ((self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
-                (self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC == 0)):
-                return "You have entered the Correct values for an Isosceles Triangle"
-            else:
-                self.print_func_one()
-                input_requirements_to_draw_triangle()
+            if self.a == 0 and self.b == 0 and self.c != 0:
+                self.a=self.b=self.c
+                return self.a,self.b,self.c,self.angA,self.angB,self.angC
 
-        if (self.type == "r".lower() or self.type == "r".upper()):
-            if ((self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
-                (self.a == 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
-                (self.a == 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0)):
-                return "You have entered the Correct values for an Right Angle Triangle"
-            else:
-                self.print_func_one()
-                input_requirements_to_draw_triangle()
 
-        if (self.type == ""):
+        if self.type == "i".lower() or self.type == "i".upper():
+            if self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB == 0 and self.angC == 0:
+                self.c=self.b
+                self.cosine_rule_find_angA()
+                self.cosine_rule_find_angB()
+                self.cosine_rule_find_angC()
+                return self.a,self.b,self.c,self.angA,self.angB,self.angC
+
+            if self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC == 0:
+                self.c=self.b
+                self.angB=(180-self.angC)/2
+                self.angC=self.angB
+                self.a=(self.b*sin(self.angA))/sin(self.angB)
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+
+        if self.type == "r".lower() or self.type == "r".upper():
+            self.angB=90
+
+            if self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB == 0 and self.angC == 0:
+                self.angA = atan(self.a / self.b)
+                self.angC=90-self.angA
+                self.c=(self.b**2-self.a**2)**0.5
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0:
+                self.angA=atan(self.c/self.a)
+                self.angC=90-self.angA
+                self.a = (self.b ** 2 - self.c ** 2) ** 0.5
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+
+        if self.type == "":
             # for SSS
-            if ((self.a != 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0) or
+            if self.a != 0 and self.b != 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC == 0:
+                self.cosine_rule_find_angB()
+                self.cosine_rule_find_angA()
+                self.cosine_rule_find_angC()
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
                 # for SAS
-                (self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB != 0 and self.angC == 0) or
-                (self.a != 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC != 0) or
-                (self.a == 0 and self.b != 0 and self.c != 0 and self.angA != 0 and self.angB == 0 and self.angC == 0) or
+            if self.a != 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB != 0 and self.angC == 0:
+                self.angA=(self.a*sin(self.angB)/self.b)
+                self.angC=180-(self.angA+self.angB)
+                self.c=(self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a != 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB == 0 and self.angC != 0:
+                self.angA = (self.a * sin(self.angC) / self.c)
+                self.angB = 180 - (self.angA + self.angC)
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+
+            if self.a == 0 and self.b != 0 and self.c != 0 and self.angA != 0 and self.angB == 0 and self.angC == 0:
+                self.angB = (self.b * sin(self.angA) / self.a)
+                self.angC = 180 - (self.angA + self.angB)
+                self.a = (self.b * sin(self.ang) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
                 # for ASA
-                (self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB != 0 and self.angC == 0) or
-                (self.a != 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB != 0 and self.angC != 0) or
-                (self.a == 0 and self.b == 0 and self.c != 0 and self.angA != 0 and self.angB == 0 and self.angC != 0) or
+            if self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB != 0 and self.angC == 0:
+                self.angC=180-(self.angA+self.angB)
+                self.a=(self.b * sin(self.angA) / sin(self.angB))
+                self.c=(self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a != 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB != 0 and self.angC != 0:
+                self.angA = 180 - (self.angC + self.angB)
+                self.b = (self.a * sin(self.angB) / sin(self.angA))
+                self.c = (self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b == 0 and self.c != 0 and self.angA != 0 and self.angB == 0 and self.angC != 0:
+                self.angB = 180 - (self.angA + self.angC)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
                 # for AAS
-                (self.a != 0 and self.b == 0 and self.c == 0 and self.angA != 0 and self.angB != 0 and self.angC == 0) or
-                (self.a == 0 and self.b == 0 and self.c != 0 and self.angA != 0 and self.angB != 0 and self.angC == 0) or
-                (self.a == 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB != 0 and self.angC != 0) or
-                (self.a == 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB != 0 and self.angC != 0) or
-                (self.a != 0 and self.b == 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC != 0) or
-                ( self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC != 0)):
-                return "You have entered the Correct values for the Triangle"
-            else:
-                self.print_func_one()
-                input_requirements_to_draw_triangle()
+
+            if self.a != 0 and self.b == 0 and self.c == 0 and self.angA != 0 and self.angB != 0 and self.angC == 0:
+                self.angC = 180 - (self.angA + self.angB)
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                self.c = (self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b == 0 and self.c != 0 and self.angA != 0 and self.angB != 0 and self.angC == 0:
+                self.angC = 180 - (self.angA + self.angB)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b != 0 and self.c == 0 and self.angA == 0 and self.angB != 0 and self.angC != 0:
+                self.angA = 180 - (self.angC + self.angB)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.c = (self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b == 0 and self.c != 0 and self.angA == 0 and self.angB != 0 and self.angC != 0:
+                self.angA = 180 - (self.angC + self.angB)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a != 0 and self.b == 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC != 0:
+                self.angB = 180 - (self.angA + self.angC)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.b = (self.c * sin(self.angB) / sin(self.angC))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+            if self.a == 0 and self.b != 0 and self.c == 0 and self.angA != 0 and self.angB == 0 and self.angC != 0:
+                self.angB = 180 - (self.angA + self.angC)
+                self.a = (self.c * sin(self.angA) / sin(self.angC))
+                self.c = (self.b * sin(self.angC) / sin(self.angB))
+                return self.a, self.b, self.c, self.angA, self.angB, self.angC
+
+
 
 
 
@@ -128,6 +261,8 @@ class Isosceles_Triangle(Triangle):
 class Scalene_Triangle(Triangle):
     pass
 
+def print_func_one():
+    print("Please Go through the instructions given and enter the correct values:")
 
 def input_requirements_to_draw_triangle():
     print("""
